f304cb2e19b5b9b7a99eec6a612818a402ad34ee
takanakahiko <takanakahiko@gmail.com>
2020-10-03 19:56:26 +0900

    scaffolding

---
+++ iOSHeadphoneMotionUdp/iOSHeadphoneMotionUdp/iOSHeadphoneMotionUdpApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSHeadphoneMotionUdpApp.swift
+//  iOSHeadphoneMotionUdp
+//
+//  Created by takanakahiko on 2020/09/27.
+//
+
+import SwiftUI
+
+@main
+struct iOSHeadphoneMotionUdpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOSHeadphoneMotionUdp/iOSHeadphoneMotionUdpTests/iOSHeadphoneMotionUdpTests.swift
@@ -0,0 +1,33 @@
+//
+//  iOSHeadphoneMotionUdpTests.swift
+//  iOSHeadphoneMotionUdpTests
+//
+//  Created by takanakahiko on 2020/09/27.
+//
+
+import XCTest
+@testable import iOSHeadphoneMotionUdp
+
+class iOSHeadphoneMotionUdpTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
