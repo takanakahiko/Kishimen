5e21d717461169fff5eda3edf366ea8b49e50376
takanakahiko <takanakahiko@gmail.com>
2020-10-14 20:19:37 +0900

    add simple udp server for  debug

---
+++ iOSHeadphoneMotionUdp/server.js
@@ -0,0 +1,34 @@
+const dgram = require('dgram');
+const server = dgram.createSocket('udp4');
+
+server.on('error', (err) => {
+  console.log(`server error:\n${err.stack}`);
+  server.close();
+});
+
+server.on('message', (msg, rinfo) => {
+  console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+});
+
+server.on('listening', () => {
+  const address = server.address();
+  console.log(`server listening ${address.address}:${address.port}`);
+});
+
+//server.bind(41234, "0.0.0.0");
+// Prints: server listening 0.0.0.0:41234
+
+ var os = require('os');
+
+var interfaces = os.networkInterfaces();
+var addresses = [];
+for (var k in interfaces) {
+    for (var k2 in interfaces[k]) {
+        var address = interfaces[k][k2];
+        if (address.family === 'IPv4' && !address.internal) {
+            addresses.push(address.address);
+        }
+    }
+}
+
+console.log(addresses);
