becf440ff0f77293102d673b5a1a872c5be4efba
takanakahiko <takanakahiko@gmail.com>
2020-11-17 01:40:08 +0900

    refactoring

--- iOSHeadphoneMotionUdp/iOSHeadphoneMotionUdp/ContentView.swift
+++ iOSHeadphoneMotionUdp/iOSHeadphoneMotionUdp/ContentView.swift
@@ -9,87 +9,6 @@ import SwiftUI
 import CoreMotion
 import Network
 
-struct Motion: Codable {
-    var attitude: Attitude
-    var rotationRate: RotationRate
-    var gravity: Acceleration
-    var userAcceleration: Acceleration
-    var magneticField: CalibratedMagneticField
-    var heading: Double
-    var sensorLocation: Int
-    var timestamp: TimeInterval
-    
-    struct Attitude: Codable {
-        var roll: Double
-        var pitch: Double
-        var yaw: Double
-        var rotationMatrix: RotationMatrix
-        var quaternion: Quaternion
-        
-        struct RotationMatrix: Codable {
-            var m11: Double
-            var m12: Double
-            var m13: Double
-            var m21: Double
-            var m22: Double
-            var m23: Double
-            var m31: Double
-            var m32: Double
-            var m33: Double
-        }
-        
-        struct Quaternion: Codable {
-            var x: Double
-            var y: Double
-            var z: Double
-            var w: Double
-        }
-    }
-    
-    struct RotationRate: Codable {
-        var x: Double
-        var y: Double
-        var z: Double
-    }
-
-    struct Acceleration: Codable {
-        var x: Double
-        var y: Double
-        var z: Double
-    }
-    
-    struct CalibratedMagneticField: Codable {
-        var field: MagneticField
-        var accuracy: Int32
-        
-        struct MagneticField: Codable {
-            var x: Double
-            var y: Double
-            var z: Double
-        }
-        
-        // これだとうまく receivedMotion.magneticField.accuracy を代入できなかった。たすけて。
-        enum MagneticFieldCalibrationAccuracy: Int32, Codable {
-            case uncalibrated = -1
-            case low = 0
-            case medium = 1
-            case high = 2
-        }
-    }
-    
-    // これだとうまく receivedMotion.sensorLocation を代入できなかった。たすけて。
-    enum SensorLocation : Int, Codable {
-        case `default` = 0
-        case headphoneLeft = -1
-        case headphoneRight = -2
-    }
-
-}
-
-struct UdpData: Codable {
-    let motion: Motion
-}
-
 struct ConnectionSetting {
     var address = "192.168.3.9"
     var port: UInt16 = 41234
@@ -200,64 +119,10 @@ class Data: ObservableObject {
     
     func sendMotion(receivedMotion: CMDeviceMotion) {
         if self.cm.isConnectionReady {
-            // このパースがヤバすぎるのでもう少しスマートに出来ないか
-            // あるいは struct 定義とともに別のソースに移せないか
-            let udpData = UdpData(
-                motion: .init(
-                    attitude: .init(
-                        roll: receivedMotion.attitude.roll,
-                        pitch: receivedMotion.attitude.pitch,
-                        yaw: receivedMotion.attitude.yaw,
-                        rotationMatrix: .init(
-                            m11: receivedMotion.attitude.rotationMatrix.m11,
-                            m12: receivedMotion.attitude.rotationMatrix.m12,
-                            m13: receivedMotion.attitude.rotationMatrix.m13,
-                            m21: receivedMotion.attitude.rotationMatrix.m21,
-                            m22: receivedMotion.attitude.rotationMatrix.m22,
-                            m23: receivedMotion.attitude.rotationMatrix.m23,
-                            m31: receivedMotion.attitude.rotationMatrix.m31,
-                            m32: receivedMotion.attitude.rotationMatrix.m32,
-                            m33: receivedMotion.attitude.rotationMatrix.m33
-                        ),
-                        quaternion: .init(
-                            x: receivedMotion.attitude.quaternion.x,
-                            y: receivedMotion.attitude.quaternion.y,
-                            z: receivedMotion.attitude.quaternion.z,
-                            w: receivedMotion.attitude.quaternion.w
-                        )
-                    ),
-                    rotationRate: .init(
-                        x: receivedMotion.rotationRate.x,
-                        y: receivedMotion.rotationRate.y,
-                        z: receivedMotion.rotationRate.z
-                    ),
-                    gravity: .init(
-                        x: receivedMotion.gravity.x,
-                        y: receivedMotion.gravity.y,
-                        z: receivedMotion.gravity.z
-                    ),
-                    userAcceleration: .init(
-                        x: receivedMotion.userAcceleration.x,
-                        y: receivedMotion.userAcceleration.y,
-                        z: receivedMotion.userAcceleration.z
-                    ),
-                    magneticField: .init(
-                        field: .init(
-                            x: receivedMotion.magneticField.field.x,
-                            y: receivedMotion.magneticField.field.y,
-                            z: receivedMotion.magneticField.field.z
-                        ),
-                        accuracy: receivedMotion.magneticField.accuracy.rawValue
-                    ),
-                    heading: receivedMotion.heading,
-                    sensorLocation: receivedMotion.sensorLocation.rawValue,
-                    timestamp: receivedMotion.timestamp
-                )
-            )
+            let udpData = ConvertMotion2Codable(motion: receivedMotion)
             let encoder = JSONEncoder()
             do {
                 let data = try encoder.encode(udpData)
-                //print(String(data: data, encoding: .utf8)!)
                 self.cm.send(content: data)
             } catch {
                 print("Unexpected error: \(error).")
@@ -315,21 +180,17 @@ struct SettingModal: View {
     var body: some View {
         NavigationView {
             VStack(alignment: .leading)  {
-
                 Text("Address").font(.headline)
                 TextField("Enter address...", text: $settingInternal.address)
                     .padding(.all)
                     .background(Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0))
                     .cornerRadius(5.0)
-                
                 Text("Port").font(.headline)
                 TextField("Enter port...", value: $settingInternal.port, formatter: NumberFormatter())
                     .padding(.all)
                     .background(Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0))
                     .cornerRadius(5.0)
-                
                 Spacer()
-                
             }
             .padding()
             .background(Color.init(red: 0.99, green: 0.99, blue: 0.99))
---
+++ iOSHeadphoneMotionUdp/iOSHeadphoneMotionUdp/Types.swift
@@ -0,0 +1,146 @@
+//
+//  Types.swift
+//  iOSHeadphoneMotionUdp
+//
+//  Created by takanakahiko on 2020/11/17.
+//
+
+import Foundation
+import CoreMotion
+
+struct Motion: Codable {
+    var attitude: Attitude
+    var rotationRate: RotationRate
+    var gravity: Acceleration
+    var userAcceleration: Acceleration
+    var magneticField: CalibratedMagneticField
+    var heading: Double
+    var sensorLocation: Int
+    var timestamp: TimeInterval
+    
+    struct Attitude: Codable {
+        var roll: Double
+        var pitch: Double
+        var yaw: Double
+        var rotationMatrix: RotationMatrix
+        var quaternion: Quaternion
+        
+        struct RotationMatrix: Codable {
+            var m11: Double
+            var m12: Double
+            var m13: Double
+            var m21: Double
+            var m22: Double
+            var m23: Double
+            var m31: Double
+            var m32: Double
+            var m33: Double
+        }
+        
+        struct Quaternion: Codable {
+            var x: Double
+            var y: Double
+            var z: Double
+            var w: Double
+        }
+    }
+    
+    struct RotationRate: Codable {
+        var x: Double
+        var y: Double
+        var z: Double
+    }
+
+    struct Acceleration: Codable {
+        var x: Double
+        var y: Double
+        var z: Double
+    }
+    
+    struct CalibratedMagneticField: Codable {
+        var field: MagneticField
+        var accuracy: Int32
+        
+        struct MagneticField: Codable {
+            var x: Double
+            var y: Double
+            var z: Double
+        }
+        
+        // これだとうまく receivedMotion.magneticField.accuracy を代入できなかった。たすけて。
+        enum MagneticFieldCalibrationAccuracy: Int32, Codable {
+            case uncalibrated = -1
+            case low = 0
+            case medium = 1
+            case high = 2
+        }
+    }
+    
+    // これだとうまく receivedMotion.sensorLocation を代入できなかった。たすけて。
+    enum SensorLocation : Int, Codable {
+        case `default` = 0
+        case headphoneLeft = -1
+        case headphoneRight = -2
+    }
+
+}
+
+struct UdpData: Codable {
+    let motion: Motion
+}
+
+
+func ConvertMotion2Codable (motion: CMDeviceMotion) -> UdpData {
+    return UdpData(
+        motion: .init(
+            attitude: .init(
+                roll: motion.attitude.roll,
+                pitch: motion.attitude.pitch,
+                yaw: motion.attitude.yaw,
+                rotationMatrix: .init(
+                    m11: motion.attitude.rotationMatrix.m11,
+                    m12: motion.attitude.rotationMatrix.m12,
+                    m13: motion.attitude.rotationMatrix.m13,
+                    m21: motion.attitude.rotationMatrix.m21,
+                    m22: motion.attitude.rotationMatrix.m22,
+                    m23: motion.attitude.rotationMatrix.m23,
+                    m31: motion.attitude.rotationMatrix.m31,
+                    m32: motion.attitude.rotationMatrix.m32,
+                    m33: motion.attitude.rotationMatrix.m33
+                ),
+                quaternion: .init(
+                    x: motion.attitude.quaternion.x,
+                    y: motion.attitude.quaternion.y,
+                    z: motion.attitude.quaternion.z,
+                    w: motion.attitude.quaternion.w
+                )
+            ),
+            rotationRate: .init(
+                x: motion.rotationRate.x,
+                y: motion.rotationRate.y,
+                z: motion.rotationRate.z
+            ),
+            gravity: .init(
+                x: motion.gravity.x,
+                y: motion.gravity.y,
+                z: motion.gravity.z
+            ),
+            userAcceleration: .init(
+                x: motion.userAcceleration.x,
+                y: motion.userAcceleration.y,
+                z: motion.userAcceleration.z
+            ),
+            magneticField: .init(
+                field: .init(
+                    x: motion.magneticField.field.x,
+                    y: motion.magneticField.field.y,
+                    z: motion.magneticField.field.z
+                ),
+                accuracy: motion.magneticField.accuracy.rawValue
+            ),
+            heading: motion.heading,
+            sensorLocation: motion.sensorLocation.rawValue,
+            timestamp: motion.timestamp
+        )
+    )
+}
